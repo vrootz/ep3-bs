15c98be21be74c9a9a0aef858fb8554c70d78dd4
schfr <78901216+vrootz@users.noreply.github.com>
2022-04-16 16:23:40 +0200

    Create kiosk.sh

---
+++ kiosk/kiosk.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# JUST UNCOMMENT WHICH NEEDED 
+firefox -kiosk -new-window file:////var/kiosk/kiosk.html
+#google-chrome -kiosk -new-window file:////var/kiosk/kiosk.html
+#chromium-browser -kiosk -new-window file:////var/kiosk/kiosk.html
